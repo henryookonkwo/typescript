d8a2652a7714aa35da7ff9f2ea4ff0a6883b8005
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-20 13:05:59 -0400

    Worked with Numbers, Strings and Booleans

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-var add = function (num1, num2) {
+var add = function (num1, num2, showResult, phrase) {
     //   if (typeof num1 !== "number" || typeof num2 !== "number") {
     //     throw new Error("Incorrect input!");
     //   }
-    return num1 + num2;
+    var result = num1 + num2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 };
-var number1 = "3";
+var number1 = 3;
 var number2 = 15.4;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "The result is ";
+var result = add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,12 +1,22 @@
-const add = (num1: number, num2: number) => {
+const add = (
+  num1: number,
+  num2: number,
+  showResult: boolean,
+  phrase: string
+) => {
   //   if (typeof num1 !== "number" || typeof num2 !== "number") {
   //     throw new Error("Incorrect input!");
   //   }
-  return num1 + num2;
+  const result = num1 + num2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 };
 
-const number1 = "3";
+const number1 = 3;
 const number2 = 15.4;
-
-const result = add(number1, number2);
-console.log(result);
+const printResult = true;
+const resultPhrase = "The result is ";
+const result = add(number1, number2, printResult, resultPhrase);
