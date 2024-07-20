c082fe23c51fceaee15a21da1fc5184a6b9c7f2d
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-20 12:03:22 -0400

    understanding types in typescript

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Code goes here!!!!!!!");
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+var number1 = 3;
+var number2 = 15.4;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("Code goes here!!!!!!!");
+const add = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+const number1 = 3;
+const number2 = 15.4;
+
+const result = add(number1, number2);
+console.log(result);
