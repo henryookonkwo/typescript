9d8001fa56b3e10f288751fefef2562f849f5bf9
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-21 14:12:25 -0400

    Including and exluding files in TS

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-//This add function is of type number
-var add = function (n1, n2) {
-    return n1 + n2;
-};
-//This printResult function is of tye void
-var printResult = function (num) {
-    console.log("Result is: ".concat(num));
-};
-printResult(add(2, 4));
-// let someItem = undefined;
-var combinedValues;
-combinedValues = add;
-// combinedValues = printResult; This gives an error because we have defined the variable to have a function type which
-//takes two number arguements and returns a value of type number and this does not meet that criteria
-console.log(combinedValues(5, 10));
-//Function Types and callbacks
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
+"use strict";
+let userInput;
+let userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+  userName = userInput;
 }
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+function generateError(message, code) {
+  throw { message: message, errorCode: code };
+  // while (true) {}
+}
+generateError("An error occurred!", 500);
