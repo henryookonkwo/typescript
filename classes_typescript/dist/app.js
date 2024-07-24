6bd2864878111759d91514b124fa65d0ca42f94c
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-24 15:44:38 -0400

    Shorthand Initialization

--- classes_typescript/dist/app.js
+++ classes_typescript/dist/app.js
@@ -1,11 +1,12 @@
 "use strict";
 class Department {
-    constructor(n) {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
         this.employees = [];
-        this.name = n;
     }
     describe() {
-        console.log(`Department: ${this.name}`);
+        console.log(`Department (${this.id}): ${this.name}`);
     }
     addEmployee(employee) {
         this.employees.push(employee);
@@ -15,10 +16,10 @@ class Department {
         console.log(this.employees);
     }
 }
-const accounting = new Department(`Accounting`);
-console.log(accounting);
+const accounting = new Department("d1", "Accounting");
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
 accounting.describe();
-accounting.addEmployee("Henry");
-accounting.addEmployee("Nechem");
+accounting.name = "NEW NAME";
 accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- classes_typescript/src/app.ts
+++ classes_typescript/src/app.ts
@@ -1,16 +1,19 @@
 class Department {
-  name: string;
+  // private id: string;
+  // private name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
   }
 
   describe(this: Department) {
-    console.log(`Department: ${this.name}`);
+    console.log(`Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
+    // validation etc
     this.employees.push(employee);
   }
 
@@ -20,14 +23,17 @@ class Department {
   }
 }
 
-const accounting = new Department(`Accounting`);
-console.log(accounting);
+const accounting = new Department("d1", "Accounting");
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+// accounting.employees[2] = 'Anna';
+
 accounting.describe();
-
-accounting.addEmployee("Henry");
-accounting.addEmployee("Nechem");
-
+accounting.name = "NEW NAME";
 accounting.printEmployeeInformation();
 
-// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
 // accountingCopy.describe();
