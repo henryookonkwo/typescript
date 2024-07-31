77498f6a06f8d766c2bab62cd5ea168d3cf25a4e
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-31 11:38:22 -0400

    Added another generic function with contraints and specific return type(tuple)

--- classes_typescript/dist/app.js
+++ classes_typescript/dist/app.js
@@ -5,4 +5,15 @@ function merge(objA, objB) {
 const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 const mergedObj2 = merge({ name: "Max" }, { age: 20 });
 console.log(mergedObj);
+function countAndDescribe(element) {
+    let descriptionText = "Got no value.";
+    if (element.length === 1) {
+        descriptionText = "Got 1 element.";
+    }
+    else if (element.length > 1) {
+        descriptionText = "Got " + element.length + " elements.";
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(["Sports", "Cooking", "Fishing", ""]));
 //# sourceMappingURL=app.js.map
