da693c6d9d3b0d7baeec87d1c5ae8445a006a625
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-31 11:16:28 -0400

    Created a generic function in TS

--- classes_typescript/dist/app.js
+++ classes_typescript/dist/app.js
@@ -1,2 +1,7 @@
 "use strict";
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergedObj);
 //# sourceMappingURL=app.js.map
---
+++ classes_typescript/src/app.ts
@@ -0,0 +1,22 @@
+// const names: Array<string> = []; // string[]
+// // names[0].split(' ');
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+
+// promise.then(data => {
+//   // data.split(' ');
+// })
+
+//Array and promise are types of generic functions
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+const mergedObj2 = merge({ name: "Max" }, { age: 20 });
+console.log(mergedObj);
