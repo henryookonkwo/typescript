dde5fa75c0f72598fbc2954e7e8717dcb9ba0c56
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-08-01 21:38:37 -0400

    Property decorators, Accessor and Parameter decorators

--- classes_typescript/src/app.ts
+++ classes_typescript/src/app.ts
@@ -1,4 +1,3 @@
-//decorator factories
 function Logger(logString: string) {
   console.log("LOGGER FACTORY");
   return function (constructor: Function) {
@@ -7,10 +6,10 @@ function Logger(logString: string) {
   };
 }
 
-function WithTemplate(template: string, hookId: string, logString: string) {
+function WithTemplate(template: string, hookId: string) {
   console.log("TEMPLATE FACTORY");
   return function (constructor: any) {
-    console.log(logString);
+    console.log("Rendering template");
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
@@ -20,8 +19,9 @@ function WithTemplate(template: string, hookId: string, logString: string) {
   };
 }
 
-@Logger("LOGGING-PERSON")
-@WithTemplate("<h1>My Person Object</h1>", "app", "Rendering template")
+// @Logger('LOGGING - PERSON')
+@Logger("LOGGING")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
 
@@ -33,3 +33,60 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// ---
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
