0fd1d811a72ee60676e69808c4bd3c421332192c
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-08-03 13:23:13 -0400

    Created and used Autobind Decorator

--- drag_and_drop_project/src/app.ts
+++ drag_and_drop_project/src/app.ts
@@ -1,3 +1,18 @@
+//Autobinder Decorator
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+//ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -32,14 +47,14 @@ class ProjectInput {
     this.configure();
     this.attach();
   }
-
+  @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
   }
 
   private configure() {
-    this.element.addEventListener("submit", this.submitHandler.bind(this));
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
