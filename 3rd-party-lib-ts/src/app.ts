53c384713d6dd986ad351ac32b8b3cf41f6b9444
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-08-05 10:26:11 -0400

    Using class-transformer: No types needed

--- 3rd-party-lib-ts/src/app.ts
+++ 3rd-party-lib-ts/src/app.ts
@@ -1,6 +1,26 @@
-import _ from "lodash";
+import "reflect-metadata";
+import { plainToInstance } from "class-transformer";
+import { Product } from "./product.model";
+// import _ from "lodash";
 
-declare var GLOBAL: any;
+// declare var GLOBAL: any;
 
-console.log(_.shuffle([1, 2, 3, 4, 5]));
-console.log(GLOBAL);
+// console.log(_.shuffle([1, 2, 3, 4, 5]));
+// console.log(GLOBAL);
+
+const products = [
+  { title: "A Carpet", price: 29.99 },
+  { title: "A Book", price: 10.99 },
+];
+
+// const p1 = new Product('A Book', 12.99);
+
+// const loadedProducts = products.map(prod => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToInstance(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
