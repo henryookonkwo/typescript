55f9bd68d9f1a01dbb785d0f8ba784510cf0acef
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-08-05 10:50:36 -0400

    Typescript-embracing: class validator

--- 3rd-party-lib-ts/src/app.ts
+++ 3rd-party-lib-ts/src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { plainToInstance } from "class-transformer";
 import { Product } from "./product.model";
+import { validate } from "class-validator";
 // import _ from "lodash";
 
 // declare var GLOBAL: any;
@@ -12,6 +13,16 @@ const products = [
   { title: "A Carpet", price: 29.99 },
   { title: "A Book", price: 10.99 },
 ];
+
+const newProd = new Product("", -5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("VALIDATION ERRORS!");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
 
 // const p1 = new Product('A Book', 12.99);
 
