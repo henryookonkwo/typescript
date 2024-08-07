177c1fc6cb1becb7e596de6dd6b2a006cd8b1416
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-08-07 18:07:57 -0400

    Finished setup & worked with types in Nodejs and Express

--- nodejs-express-ts/dist/app.js
+++ nodejs-express-ts/dist/app.js
@@ -6,3 +6,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.listen(3000);
+console.log("testing");
--- nodejs-express-ts/src/app.ts
+++ nodejs-express-ts/src/app.ts
@@ -3,3 +3,4 @@ import express from "express";
 const app = express();
 
 app.listen(3000);
+console.log("testing");
